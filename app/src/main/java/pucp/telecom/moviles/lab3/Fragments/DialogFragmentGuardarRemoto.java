8d20c221ea94d0e3ea382bfcfb9e148f98a053bf
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-06-22 09:38:42 -0500

    guardar remoto + GPS

--- app/src/main/java/pucp/telecom/moviles/lab3/Fragments/DialogFragmentGuardarRemoto.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/Fragments/DialogFragmentGuardarRemoto.java
@@ -1,14 +1,52 @@
 package pucp.telecom.moviles.lab3.Fragments;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
+import static pucp.telecom.moviles.lab3.Util.isInternetAvailable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DialogFragmentGuardarRemoto extends androidx.fragment.app.DialogFragment {
+
+
+    private String apiKey = "lpc4qJWzB4WwelLmtRrfLkueV1L3wJ";
+
+    private double latitud;
+    private double longitud;
+    private int tiempo;
+    private double[] mediciones;
+
+
+    public DialogFragmentGuardarRemoto(int tiempo, double[] mediciones, double latitud, double longitud) {
+        this.tiempo=tiempo;
+        this.mediciones=mediciones;
+        this.latitud=latitud;
+        this.longitud=longitud;
+    }
 
     @NonNull
     @Override
@@ -19,6 +57,44 @@ public class DialogFragmentGuardarRemoto extends androidx.fragment.app.DialogFra
                 .setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
+                        if (isInternetAvailable(DialogFragmentGuardarRemoto.this.getContext())) {
+                            RequestQueue requestQueue = Volley.newRequestQueue(DialogFragmentGuardarRemoto.this.getContext());
+
+                            String url = " http://ec2-34-234-229-191.compute-1.amazonaws.com:5000/saveData";
+                            StringRequest stringRequest = new StringRequest(StringRequest.Method.POST, url, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.d("Crear", error.getLocalizedMessage());
+                                }
+                            }) {
+                                @Override
+                                public Map<String, String> getHeaders() throws AuthFailureError {
+                                    Map<String, String> headers = new HashMap<>();
+                                    headers.put("api-key", apiKey);
+                                    return headers;
+                                }
+
+                                @Override
+                                protected Map<String, String> getParams() throws AuthFailureError {
+                                    Map<String, String> params = new HashMap<>();
+
+                                    params.put("tiempo", String.valueOf(tiempo));
+                                    params.put("mediciones", Arrays.toString(mediciones));
+                                    params.put("latitud", String.valueOf(latitud));
+                                    params.put("longitud", String.valueOf(longitud));
+
+                                    return params;
+                                }
+                            };
+                            requestQueue.add(stringRequest);
+                        }
+
+
                     }
                 })
                 .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -28,4 +104,7 @@ public class DialogFragmentGuardarRemoto extends androidx.fragment.app.DialogFra
                 });
         return builder.create();
     }
+
+
+
 }
--- app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
@@ -1,17 +1,33 @@
 package pucp.telecom.moviles.lab3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import pucp.telecom.moviles.lab3.Fragments.DialogFragmentEjemplo;
 import pucp.telecom.moviles.lab3.Fragments.DialogFragmentGuardarLocal;
 import pucp.telecom.moviles.lab3.Fragments.DialogFragmentGuardarRemoto;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
+    private double currentLocationLatitud;
+    private double currentLocationLongitud;
+    private int tiempo;
+    private double[] mediciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +45,51 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonGuardarRemoto).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DialogFragmentGuardarRemoto dialogFragment = new DialogFragmentGuardarRemoto();
+
+                getGPSValues();
+
+                tiempo = 10;
+                mediciones = new double[]{60.5, 61.4, 64.3};
+
+                DialogFragmentGuardarRemoto dialogFragment = new DialogFragmentGuardarRemoto(tiempo, mediciones, currentLocationLatitud, currentLocationLongitud);
                 dialogFragment.show(getSupportFragmentManager(), "guardarRemoto");
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void getCurrentLocation() {
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocationLatitud = location.getLatitude();
+                    currentLocationLongitud = location.getLongitude();
+                }
+            }
+        });
+    }
+
+    private void getGPSValues() {
+        client = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else { //Request for permission
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+
+        }
+    }
+
 }
