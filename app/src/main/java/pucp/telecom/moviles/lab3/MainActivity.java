154e26634756a0bd9ccaf04d4df903923e7a7916
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-06-22 08:58:31 -0500

    fragments

--- app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
@@ -1,8 +1,15 @@
 package pucp.telecom.moviles.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import pucp.telecom.moviles.lab3.Fragments.DialogFragmentEjemplo;
+import pucp.telecom.moviles.lab3.Fragments.DialogFragmentGuardarLocal;
+import pucp.telecom.moviles.lab3.Fragments.DialogFragmentGuardarRemoto;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonGuardarLocal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragmentGuardarLocal dialogFragment = new DialogFragmentGuardarLocal();
+                dialogFragment.show(getSupportFragmentManager(), "guardarLocal");
+            }
+        });
+
+        findViewById(R.id.buttonGuardarRemoto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragmentGuardarRemoto dialogFragment = new DialogFragmentGuardarRemoto();
+                dialogFragment.show(getSupportFragmentManager(), "guardarRemoto");
+            }
+        });
     }
 }
